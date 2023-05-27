73aac7fba97e5c8b2ce9f05137ee49f99018455a
c5rogers <natenaelhaylemariam@gmail.com>
2023-05-27 09:44:44 -0400

    some modification

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,22 @@
 import { useQuery } from '@vue/apollo-composable'
 import GridCard from '../components/GridCard.vue';
 import countryQuery from '../querys/country.gql'
+import { ref } from 'vue';
+
+
+const {result}=useQuery(countryQuery)
+
+
+
+const search=ref('')
 
 const handleSubmition=()=>{
 
 }
-const {result}=useQuery(countryQuery)
+
+const filterFild=()=>{
+  result=result.countries.filter((country)=>country.name.includes(search.value) || country.language.includes(search.value)||country.phone.includes(search.value)||country.capital.includes(search.value))
+}
 </script>
 
 <template>
@@ -14,7 +25,7 @@ const {result}=useQuery(countryQuery)
     <div class="w-full flex items-center h-fit">
         <form @submit.prevent="handleSubmition">
             <div class="relative w-64">
-                <input type="text" name="search" placeholder="Search Country..." class="w-full border  outline-none pl-10 pr-2 py-2 shadow-sm rounded-md">
+                <input type="text" name="search" placeholder="Search Country..." class="w-full border  outline-none pl-10 pr-2 py-2 shadow-sm rounded-md" v-model="search" @change="filterFild">
                 <i class="fa-solid fa-search absolute top-3 text-gray-500 left-2"></i>
             </div>
         </form>
